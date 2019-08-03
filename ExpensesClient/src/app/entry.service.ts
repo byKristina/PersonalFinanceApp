f036370d9565e8853e0d25b787d8521dbc25258c
krispro <47563479+krispro@users.noreply.github.com>
2019-08-03 19:01:31 +0200

    Getting started with Angular, enable CORS

---
+++ ExpensesClient/src/app/entry.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  baseUrl: string = 'http://localhost:53754/api/entries/'
+
+  constructor(private http:HttpClient) { }
+
+  getAll(){
+    return this.http.get(this.baseUrl);
+  }
+
+}
+
+
